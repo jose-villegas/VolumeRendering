5dc67d16beb63621692c6a6841a7571220c326dd
jose-villegas <polonoise@gmail.com>
2014-09-30 21:40:28 -0430

    Agregado Contexto para Renderizar en OpenGL

--- FGC_T3/Main.cpp
+++ FGC_T3/Main.cpp
@@ -1,13 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 #include <iostream>
+
+#define WINDOW_OFFSET 200
+
+// OpenGL Setup Before Rendering to Context
+void OpenGL_Setup();
+// SFML Context Settings for OpenGL Rendering
+sf::ContextSettings OpenGL_WindowContextSetup();
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Clock clock;
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    desktop.height = desktop.height - WINDOW_OFFSET;
+    desktop.width = desktop.width - WINDOW_OFFSET;
+    sf::RenderWindow window(desktop, "SFML works!", sf::Style::Default, OpenGL_WindowContextSetup());
+    window.setActive(true);
+    // Setup OpenGL to Current Context
+    OpenGL_Setup();
+    bool running = true;
 
-    while (window.isOpen())
+    while (running)
     {
         sf::Event event;
 
@@ -15,14 +30,84 @@ int main()
         {
             if (event.type == sf::Event::Closed)
             {
-                window.close();
+                running = false;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
             }
         }
 
-        window.clear();
-        window.draw(shape);
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Render OpenGL
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glTranslatef(0.f, 0.f, -7.f);
+        glRotatef(clock.getElapsedTime().asSeconds() * 50, 1.f, 0.f, 0.f);
+        glRotatef(clock.getElapsedTime().asSeconds() * 30, 0.f, 1.f, 0.f);
+        glRotatef(clock.getElapsedTime().asSeconds() * 90, 0.f, 0.f, 1.f);
+        glBegin(GL_QUADS);
+        glColor3f(0.0f, 1.0f, 0.0f);	// Color Blue
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Top Right Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Top Left Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Bottom Left Of The Quad (Top)
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Bottom Right Of The Quad (Top)
+        glColor3f(1.0f, 0.5f, 0.0f);	// Color Orange
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Top Right Of The Quad (Bottom)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Top Left Of The Quad (Bottom)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Quad (Bottom)
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Bottom Right Of The Quad (Bottom)
+        glColor3f(1.0f, 0.0f, 0.0f);	// Color Red
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Top Right Of The Quad (Front)
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Top Left Of The Quad (Front)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Bottom Left Of The Quad (Front)
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Bottom Right Of The Quad (Front)
+        glColor3f(1.0f, 1.0f, 0.0f);	// Color Yellow
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Top Right Of The Quad (Back)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Left Of The Quad (Back)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Bottom Left Of The Quad (Back)
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Bottom Right Of The Quad (Back)
+        glColor3f(0.0f, 0.0f, 1.0f);	// Color Blue
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Top Right Of The Quad (Left)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Top Left Of The Quad (Left)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Quad (Left)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Bottom Right Of The Quad (Left)
+        glColor3f(1.0f, 0.0f, 1.0f);	// Color Violet
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Top Right Of The Quad (Right)
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Top Left Of The Quad (Right)
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Bottom Left Of The Quad (Right)
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Bottom Right Of The Quad (Right)
+        glEnd();
+        // End Frame
         window.display();
     }
 
     return 0;
+}
+
+sf::ContextSettings OpenGL_WindowContextSetup()
+{
+    sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antialiasingLevel = 2;
+    settings.majorVersion = 3;
+    settings.minorVersion = 0;
+    return settings;
+}
+
+void OpenGL_Setup()
+{
+    // Set color and depth clear value
+    glClearDepth(1.f);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+    // Enable Z-buffer read and write
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    // Setup a perspective projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(90.f, 1.f, 1.f, 500.f);
 }
